584ad2eba5143f7990dd46345dca69b5c8f93581
NivethaR02 <rnivetha435@gmail.com>
2025-07-01 22:33:31 +0530

    added sample2 class

---
+++ git1/src/git1/Sample2.java
@@ -0,0 +1,10 @@
+package git1;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi da mahesh");
+	}
+
+}
--- git1/src/git1/sample1.java
+++ git1/src/git1/sample1.java
@@ -3,5 +3,6 @@ package git1;
 public class sample1 {
 	public static void main (String [] args) {
 		System.out.println("hi da ramu");
+		
 	}
 }
